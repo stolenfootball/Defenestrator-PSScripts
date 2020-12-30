a108c73ffbc9a3e11a43d5d3627f67ea7e070845
Jeremy Dunn <jeremy.dunn315@gmail.com>
2020-12-30 08:52:09 -0500

    Add the disable_background_processes.ps1 snippet

---
+++ Snippets/disable_background_processes.ps1
@@ -0,0 +1,26 @@
+
+# Disable Telemetry and Data Collection
+Reg Add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /T REG_DWORD /V "AllowTelemetry" /D 0 /F
+
+# Disable targeted advertising 
+Reg Add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\AdvertisingInfo" /T REG_DWORD /V "DisabledByGroupPolicy" /D 1 /F
+
+# Disable Cortana
+Reg Add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /T REG_DWORD /V "Allow Cortana" /D 1 /F
+
+# Turn off Website Access of LAnguage List
+Reg Add "HKEY_CURRENT_USER\Control Panel\International\User Profile" /T REG_DWORD /V "HttpAcceptLanguageOptOut" /D 1 /F
+
+# Turn of Windows Track app launches
+Reg Add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /T REG_DWORD /V "Start_TrackProgs" /D 0 /F
+
+# Turn off Suggested content in the settings app
+Reg Add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /T REG_DWORD /V "SubscribedContent-338393Enabled" /D 0 /F
+Reg Add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /T REG_DWORD /V "SubscribedContent-353694Enabled" /D 0 /F
+Reg Add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /T REG_DWORD /V "SubscribedContent-353696Enabled" /D 0 /F
+
+# Turn off Activity History
+Reg Add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /T REG_DWORD /V "PublishUserActivities" /D 0 /F
+
+# Turn off Tailored Experience based off diagnostic data
+Reg Add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Privacy" /T REG_DWORD /V "TailoredExperiencesWithDiagnosticDataEnabled" /D 0 /F
