32f13ed08298bef0950e860bef1826b3c6022d6c
Jeremy Dunn <jeremy.dunn315@gmail.com>
2021-06-02 09:53:50 -0400

    Add line to output full package name in list-app-appx-packages

--- Snippets/list_all_appx_packages.ps1
+++ Snippets/list_all_appx_packages.ps1
@@ -1,8 +1,9 @@
 ﻿$User = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $Packages = Get-AppxPackage -User "$User" | Select Name
+Write-Output $Packages
 
-foreach($Element in $Packages) {
-$MidElement = $Element.Name -Replace 'Microsoft.', ''
-$CleanElement = $MidElement -Replace 'Windows.', ''
-    Write-Output $CleanElement
-}
+# foreach($Element in $Packages) {
+# $MidElement = $Element.Name -Replace 'Microsoft.', ''
+# $CleanElement = $MidElement -Replace 'Windows.', ''
+#     Write-Output $CleanElement
+# }
